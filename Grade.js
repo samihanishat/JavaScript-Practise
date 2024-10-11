ab198711e0115a84c4029a3e97b1fc7122292fbd
samia <samianishatox@gmail.com>
2024-10-11 20:32:59 +0600

    Added Grade Calculator

---
+++ Grade.js
@@ -0,0 +1,16 @@
+let mark = 60;
+if (mark >= 90){
+    console.log("Your Grade is A");
+}
+else if (mark >= 80 && mark <90){
+    console.log("Your Grade is B");
+}
+else if (mark >= 70 && mark <80){
+    console.log("Your Grade is C");
+}
+else if (mark >= 60 && mark <70){
+    console.log("Your Grade is D");
+}
+else{
+    console.log("Your Grade is F");
+}
