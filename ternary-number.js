5dd4df96ac02c1baa270e164f9b4ad80d6c7dc80
samia <samianishatox@gmail.com>
2024-10-11 21:05:46 +0600

    Added ternary number

---
+++ ternary-number.js
@@ -0,0 +1,19 @@
+let num1 = 12;
+let num2 = 11;
+let result
+num1 > num2 ? result = num1 * 2 : result = num1 + num2;
+console.log(result);
+
+/** 
+ * if(num1 > num2){
+ *  result =
+ * }
+*/
+if (num1 > num2) {
+    result = num1*2;
+    }
+    else{
+        result = num1 + num2;
+    }
+console.log(result);
+
